2ba57e662e81c862f10e5a5edb29ebb507bcbddc
CarlosFarlos <carlos98oquendo@gmail.c>
2023-11-11 20:23:02 -0500

    Updated set serializing and retrieval from database

--- app/src/main/java/edu/bloomu/budgetapp/AddFragment.java
+++ app/src/main/java/edu/bloomu/budgetapp/AddFragment.java
@@ -37,10 +37,6 @@ public class AddFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public AddFragment() {
         // Required empty public constructor
     }
@@ -64,17 +60,15 @@ public class AddFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
+        assert getArguments() != null;
         String reference = getArguments().getString(ARG_PARAM1);
+        assert reference != null;
         userRef = FirebaseDatabase.getInstance().getReference()
                 .child("users").child(reference);
         budgets = Budget.getBudgets(userRef);
